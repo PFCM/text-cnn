817425b6a243b10f48a2ae4896513219a1b3ef49
pfcm <pfcmathews@gmail.com>
2017-09-06 09:47:43 +1200

    start actually making things

---
+++ models/__init__.py
@@ -0,0 +1,1 @@
+from models.ar_cnn import *
---
+++ train.py
@@ -0,0 +1,11 @@
+"""
+Train a model, either to generate or upsample.
+"""
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+import tensorflow as tf
+
+import read
+import models
